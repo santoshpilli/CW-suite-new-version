8a091625d23ce9219d2a0b92be9d65d4ed475ca6
aishwarya <aishwarya.k@exceloid.com>
2024-07-03 14:52:36 +0530

    Added SSG

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "14e85cf6b37e3939"; }
+/******/ 		__webpack_require__.h = function() { return "5a09eb7c03e0a881"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/[...index].js
+++ pages/[...index].js
@@ -1,156 +1,43 @@
-
-// import React from "react";
-// import PageSEO from "../components/common/PageSEO";
-// import { MetaData } from "../components/common/Helper";
-// import RenderSections from "../components/renderSections";
-// // import menuData from './menudata.json'
-// import menuData from './menu.json'
-// import LoadingAnimation from "../custom/animate";
-
-// import Layout from "../hoc/Layout";
-// import axios from "axios";
-// import Custom404 from "./404"
-// import CustomPages from "../custom";
-
-// export async function getServerSideProps(context) {
-//   const { query } = context;
-//   const pathSegments = query.index || [];
-//   const path = `/${pathSegments.join("/")}`;
-
-
-//   let matchedMenuItem = null;
-//   for (const menuItem of menuData) {
-//     if (menuItem.slug === path) {
-//       matchedMenuItem = menuItem;
-//       break;
-//     }
-//     if (menuItem.children) {
-//       for (const childItem of menuItem.children) {
-//         if (childItem.slug === path) {
-//           matchedMenuItem = childItem;
-//           break;
-//         }
-//         if (childItem.subchildren) {
-//           for (const subChildItem of childItem.subchildren) {
-//             if (subChildItem.slug === path) {
-//               matchedMenuItem = subChildItem;
-//               break;
-//             }
-//           }
-//           if (matchedMenuItem) {
-//             break;
-//           }
-//         }
-//       }
-//       if (matchedMenuItem) {
-//         break;
-//       }
-//     }
-//   }
-//   const { type, menuId, slug } = matchedMenuItem || {};
-
-//   if (!matchedMenuItem) {
-//     return {
-//       notFound: true,
-//     };
-//   }
-
-//   if (type === 'custom') {
-//     return {
-//       props: {
-//         type,
-//         data: null,
-//         slug
-//       },
-//     };
-//   } else {
-//     try {
-//       let tempSlug = path.substring(1);
-
-//       const finalUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/api/pageData?slug=${tempSlug}`;
-//       const headers = {
-//         'Content-Type': 'application/json',
-//       };
-//       const response = await axios.get(finalUrl, headers);
-
-//       const data = response.data?.documents[0];
-
-//       if (!data) {
-//         return {
-//           notFound: true,
-//         };
-//       }
-
-//       return {
-//         props: {
-//           type,
-//           data,
-//           slug
-//         },
-//       };
-//     } catch (error) {
-//       console.error('Error fetching data:', error);
-//       return {
-//         notFound: true,
-//       };
-//     }
-//   }
-// }
-
-
-// export default function Home({ type, data, slug }) {
-//   if (!data && type !== 'custom') {
-//     return <Custom404 />;
-//   }
-
-//   return (
-//     <>
-//       <PageSEO
-//         title={data?.pageTitle}
-//         canonicalTag={""}
-//         metatitleTag={MetaData[0].titleTag}
-//         metaDes={MetaData[0].metaDescription}
-//       />
-
-//       {data === null && type === "page" ? <Layout> <LoadingAnimation /> </Layout> :
-//         type === 'page' && <RenderSections data={data} />
-//       }
-
-//       {type === 'post' && <PostComponent />}
-//       {type === 'form' && <Layout><h2>Form component</h2></Layout>}
-//       {type === 'custom' && <CustomPages type={type} slug={slug} />}
-//       {data === null && type !== 'custom' && <Custom404 />}
-//     </>
-//   );
-// }
-
-
-
+// [...index].js
 
 import React from "react";
-import useSWR from "swr";
 import PageSEO from "../components/common/PageSEO";
 import { MetaData } from "../components/common/Helper";
 import RenderSections from "../components/renderSections";
 import menuData from './menu.json';
 import LoadingAnimation from "../custom/animate";
 import Layout from "../hoc/Layout";
+import axios from "axios";
 import Custom404 from "./404";
 import CustomPages from "../custom";
-import axios from "axios";
 
+export async function getStaticPaths() {
+  const paths = menuData.map(menuItem => {
+    const paths = [];
+    if (menuItem.children) {
+      menuItem.children.forEach(childItem => {
+        if (childItem.subchildren) {
+          childItem.subchildren.forEach(subChildItem => {
+            paths.push({ params: { index: subChildItem.slug.split('/').filter(Boolean) } });
+          });
+        } else {
+          paths.push({ params: { index: childItem.slug.split('/').filter(Boolean) } });
+        }
+      });
+    } else {
+      paths.push({ params: { index: menuItem.slug.split('/').filter(Boolean) } });
+    }
+    return paths;
+  }).flat();
 
-const fetcher = async (url) => {
-  const response = await axios.get(url, {
-    headers: { 'Content-Type': 'application/json' },
-  });
-  if (!response.data || !response.data.documents[0]) {
-    throw new Error('No data found');
-  }
-  return response.data.documents[0];
-};
+  return { paths, fallback: 'blocking' };
+}
 
-const findMenuItem = (path) => {
+export async function getStaticProps(context) {
+  const { params } = context;
+  const pathSegments = params.index || [];
+  const path = `/${pathSegments.join("/")}`;
+
   let matchedMenuItem = null;
   for (const menuItem of menuData) {
     if (menuItem.slug === path) {
@@ -180,44 +67,61 @@ const findMenuItem = (path) => {
       }
     }
   }
-  return matchedMenuItem;
-};
 
-export async function getServerSideProps(context) {
-  const { query } = context;
-  const pathSegments = query.index || [];
-  const path = `/${pathSegments.join("/")}`;
-
-
-  const matchedMenuItem = findMenuItem(path);
-  
-
-  const { type, slug } = matchedMenuItem || {};
-
-  return {
-    props: {
-      path,
-      type,
-      slug,
-      matchedMenuItem,
-    },
-  };
-}
-
-export default function Home({ path, type, slug, matchedMenuItem }) {
-  const { data, error } = useSWR(
-    matchedMenuItem && type !== 'custom' ? `${process.env.NEXT_PUBLIC_BASE_URL}/api/pageData?slug=${path.substring(1)}` : null,
-    fetcher
-  );
-
- 
+  const { type, menuId, slug } = matchedMenuItem || {};
 
   if (!matchedMenuItem) {
-    return <Custom404 />;
+    return {
+      notFound: true,
+    };
   }
 
-  if (error) {
-    console.error('Error fetching data:', error);
+  if (type === 'custom') {
+    return {
+      props: {
+        type,
+        data: null,
+        slug
+      },
+      revalidate: 86400, // Revalidate every 24 hours
+    };
+  } else {
+    try {
+      let tempSlug = path.substring(1);
+      const finalUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/api/pageData?slug=${tempSlug}`;
+      const headers = {
+        'Content-Type': 'application/json',
+      };
+      const response = await axios.get(finalUrl, headers);
+
+      const data = response.data?.documents[0];
+
+      if (!data) {
+        return {
+          notFound: true,
+        };
+      }
+
+      return {
+        props: {
+          type,
+          data,
+          slug
+        },
+        revalidate: 86400, // Revalidate every 24 hours
+      };
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      return {
+        notFound: true,
+        revalidate: 86400, // Revalidate every 24 hours
+      };
+    }
+  }
+}
+
+export default function Home({ type, data, slug }) {
+  if (!data && type !== 'custom') {
     return <Custom404 />;
   }
 
@@ -230,19 +134,17 @@ export default function Home({ path, type, slug, matchedMenuItem }) {
         metaDes={MetaData[0].metaDescription}
       />
 
-      {!data && type === "page" ? <Layout> <LoadingAnimation /> </Layout> :
+      {data === null && type === "page" ? <Layout> <LoadingAnimation /> </Layout> :
         type === 'page' && <RenderSections data={data} />
       }
 
       {type === 'post' && <PostComponent />}
       {type === 'form' && <Layout><h2>Form component</h2></Layout>}
       {type === 'custom' && <CustomPages type={type} slug={slug} />}
-      {!data && type !== 'custom' && <Custom404 />}
+      {data === null && type !== 'custom' && <Custom404 />}
     </>
   );
 }
-
-
 
 
 
